7ae353f5c7dbbe3522fb2a8c1558ed97557d2526
Arnav <arnavbgd@gmail.com>
2026-05-02 20:48:33 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".card").forEach(card => {
+    let pos = card.getBoundingClientRect().top;
+    if(pos < window.innerHeight - 100){
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
